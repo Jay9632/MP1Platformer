90de5195e36185db336080df75f3971da693a82e
Javier <Jay14539@gmail.com>
2023-07-08 08:32:06 -0700

    background scrolling

--- index.js
+++ index.js
@@ -35,10 +35,10 @@
     }
 
     class Platform {
-        constructor() {
+        constructor({x, y }) {
             this.position = {
-                x: 200,
-                y: 100
+                x: x ,
+                y: y
             }
 
             this.width = 200
@@ -54,7 +54,11 @@
 
 
     const player = new Player()
-    const platform = new Platform()
+    const platforms = [new Platform({
+        x: 200, y: 100
+    }), new Platform({
+        x:200, y:500
+    })]
 
     const keys = {
         right:{
@@ -70,18 +74,34 @@
         requestAnimationFrame(animate)
         c.clearRect(0, 0, canvas.width, canvas.height)
         player.update()
-        platform.draw()
+        platforms.forEach((platform) => {
+            platform.draw()
+        })
 
-        if(keys.right.pressed) {
+        if(keys.right.pressed && player.position.x < 400) {
             player.velocity.x = 5
-        } else if (keys.left.pressed) {
+        } else if (keys.left.pressed && player.position.x > 100) {
             player.velocity.x = -5
-        } else player.velocity.x = 0
+        } else {
+            player.velocity.x = 0
 
-        //platform collision 
+            if (keys.right.pressed) {
+                platforms.forEach((platform) => {
+                    platform.position.x -= 5
+                })
+                } else if (keys.left.pressed) {
+                platforms.forEach((platform) => {
+                    platform.position.x += 5
+                })
+            }
+        }
+
+        //platform collision
+        platforms.forEach((platform) => { 
         if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width ) {
             player.velocity.y = 0
         }
+    })
     }
 
     animate()
