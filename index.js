766bb94ea12d21855651be3a8fe37c7586e350cc
Javier <Jay14539@gmail.com>
2023-07-08 07:38:28 -0700

    movement

--- index.js
+++ index.js
@@ -26,6 +26,7 @@
         update() {
             this.draw()
             this.position.y += this.velocity.y
+            this.position.x += this.velocity.x
 
             if (this.position.y +this.height +this.velocity.y <= canvas.height)
             this.velocity.y += gravity
@@ -34,12 +35,75 @@
     }
 
     const player = new Player()
+    const keys = {
+        right:{
+            pressed: false
+        },
+        left: {
+            pressed: false
+        }
+    }
     player.draw()  
 
     function animate() {
         requestAnimationFrame(animate)
         c.clearRect(0, 0, canvas.width, canvas.height)
         player.update()
+
+        if(keys.right.pressed) {
+            player.velocity.x = 5
+        } else if (keys.left.pressed) {
+            player.velocity.x = -5
+        } else player.velocity.x = 0
     }
 
     animate()
+
+    window.addEventListener('keydown', ({ keyCode }) => {
+        switch (keyCode) {
+            case 87:
+                console.log('up')
+                player.velocity.y -= 10
+                break
+
+                case 83:
+                console.log('down')
+                break
+
+                case 65:
+                console.log('left')
+                keys.left.pressed = true
+                break
+
+                case 68:
+                console.log('right')
+                keys.right.pressed = true
+                break
+        }
+
+        console.log(keys.right.pressed)
+    })
+
+        window.addEventListener('keyup', ({ keyCode }) => {
+            switch (keyCode) {
+                case 87:
+                    console.log('up')
+                    player.velocity.y -= 10
+                    break
+    
+                    case 83:
+                    console.log('down')
+                    break
+    
+                    case 65:
+                    console.log('left')
+                    keys.left.pressed = false
+                    break
+    
+                    case 68:
+                    console.log('right')
+                    keys.right.pressed = false
+                    break
+            }
+            console.log(keys.right.pressed)
+    })
